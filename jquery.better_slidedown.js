161bfe4af3a700373005999753329c8aa269bbba
Jimmy Mabey <jimmy@znanja.com>
2012-03-22 14:30:57 -0300

    Improve documentation + avoid naming conflicts

--- jquery.better_slidedown.js
+++ jquery.better_slidedown.js
@@ -1,5 +1,5 @@
 /**
- * Replacement slideDown function.
+ * Replacement slideDown() implementation for jQuery.
  *
  * jQuery's default function jumps around if there are any floated elements in
  * the animating element.  For some reason, it thinks the height is larger than
@@ -7,65 +7,79 @@
  * because it takes into account the real height of the element, including
  * margin and padding.
  *
- * @param {mixed} duration     How long animation will run
- * @param {string} easing      Animation easing function name
- * @param {function} callback  Called after animation is complete
- * @returns {jQuery}  Chainable
+ * @author znanja, inc
  */
-$.fn.slideDown = function(duration, easing, callback)
+
+(function($)
 {
-	var options = $.speed(duration, easing, callback);
-
-	return this.queue(function(next)
+	/**
+	 * Slide down the matching elements.
+	 *
+	 * @param {mixed} duration     How long animation will run
+	 * @param {string} easing      Animation easing function name
+	 * @param {function} callback  Called after animation is complete
+	 * @returns {jQuery}  Chainable
+	 */
+	$.fn.slideDown = function(duration, easing, callback)
 	{
-		var element = $(this);
+		var options = $.speed(duration, easing, callback);
 
-		if (element.is(':visible'))
+		return this.queue(function(next)
 		{
-			// Element is already slid down
-			options.complete.call(this);
-			return;
-		}
+			var element = $(this);
 
-		// Temporarily show the element so we can correctly determine its height
-		// -- this won't be visible to the user becase we're animating soon after
-		element.show();
-
-		// Get element properties that contribute to heights
-		var properties = {
-			height:           element.height(),
-			'margin-top':     element.css('margin-top'),
-			'margin-bottom':  element.css('margin-bottom'),
-			'padding-top':    element.css('padding-top'),
-			'padding-bottom': element.css('padding-bottom')
-		};
-
-		$.each(properties, function(key, value)
-		{
-			// Filter out non-numeric properties, since we cannot animate those
-			if (isNaN(parseFloat(value)))
+			if (element.is(':visible'))
 			{
-				delete properties[key];
-			}
-		});
-
-		// Zero each property
-		for (var property in properties)
-		{
-			element.css(property, 0);
-		}
-
-		element.animate(properties, options.duration, options.easing, function()
-		{
-			// Remove inline CSS
-			for (var prop in properties)
-			{
-				element.css(prop, '');
+				// Element is already slid down
+				options.complete.call(this);
+				return;
 			}
 
-			options.complete.call(this);
-		});
+			// Temporarily show the element so we can correctly determine its
+			// height -- this won't be visible to the user becase we're
+			// animating soon after
+			element.show();
 
-		next();
-	});
-};
+			// Element properties that contribute to heights
+			var properties = {
+				'height':         element.height(),
+				'margin-top':     element.css('margin-top'),
+				'margin-bottom':  element.css('margin-bottom'),
+				'padding-top':    element.css('padding-top'),
+				'padding-bottom': element.css('padding-bottom')
+			};
+
+			$.each(properties, function(key, value)
+			{
+				// Filter out non-numeric properties, since we cannot animate
+				// those
+				if (isNaN(parseFloat(value)))
+				{
+					delete properties[key];
+				}
+			});
+
+			// Zero each property
+			for (var property in properties)
+			{
+				element.css(property, 0);
+			}
+
+			element.animate(
+				properties, options.duration, options.easing,
+				function()
+				{
+					// Remove leftover inline CSS from animation
+					for (var prop in properties)
+					{
+						element.css(prop, '');
+					}
+
+					options.complete.call(this);
+				}
+			);
+
+			next();
+		});
+	};
+})(jQuery);
